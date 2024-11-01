d2b62e6fa8e67e7a6a4def1cfc6b0bde676b55ca
cubitdxb <cubitdxb@gmail.com>
2024-11-01 11:00:06 +0400

    updae comapny

--- company_customisation/models/company.py
+++ company_customisation/models/company.py
@@ -13,7 +13,6 @@ class CubitCompany(models.Model):
     expired_template_id = fields.Many2one('mail.template', string='Expired Email Template')
     rml_footer = fields.Html('Report Footer')
     rml_ls_footer = fields.Html('Report LS Footer')
-    # rml_ls_footer = fields.Html('Report LS Footer')
     cubit_id = fields.Integer('Cubit Id')
     proposal_header = fields.Binary(string='Proposal Banner')
 
