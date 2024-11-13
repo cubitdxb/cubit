878a25347f5c564e9a1ae9a28c31d636a3fcb4b9
cubitdxb <cubitdxb@gmail.com>
2024-11-14 01:45:24 +0400

    push

--- vox_task_template/models/technical_team_assignment.py
+++
@@ -1,7 +0,0 @@
-from odoo import models, fields, api, _
-
-#
-# class TechnicalTeamAssignment(models.Model):
-#     _inherit = 'project.task'
-#
-#     sale_id = fields.Many2one('sale.order', 'Sale Order')
